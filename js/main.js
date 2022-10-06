12bf68e46a855cb889d6db47dbb13ab49c86fc00
damnfantasy <damnfantasy@list.ru>
2022-10-06 20:40:05 +0700

    module2-task1(fix)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-const getRandomInteger = (min, max) => Math.floor(Math.random () * (max - min + 1)) + min;
+const getRandomInteger = (min, max) => {
+  if (typeof(min) !== 'number' || typeof(max) !== 'number' || min < 0 || max < 0){
+    return NaN;
+  }
+  if (min >= max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  return Math.floor(Math.random () * (max - min + 1)) + min;
+};
+
+
 const checkCommentaryLength = (str, maxLength) => str.length <= maxLength;
+
 getRandomInteger(1,5);
 checkCommentaryLength('sadcscacascas', 40);
-
